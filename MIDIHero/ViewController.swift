bfd304f6cf6ab093807356b35a82b636ae41718c
DelbyPicnic <gordonswan@me.com>
2018-06-14 21:09:46 +0100

    Initial Commit

---
+++ MIDIHero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MIDIHero
+//
+//  Created by Gordon Swan on 14/06/2018.
+//  Copyright © 2018 Gordon Swan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
