1792d2e526bbb2f46bb9d38ae6542bb4a1fef563
Gordon Swan <hello@blackfeet.uk>
2021-01-15 23:08:02 +0000

    Initial Commit

---
+++ MIDIHero/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MIDIHero
+//
+//  Created by Gordon Swan on 15/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MIDIHero/MIDIHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDIHeroApp.swift
+//  MIDIHero
+//
+//  Created by Gordon Swan on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MIDIHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
